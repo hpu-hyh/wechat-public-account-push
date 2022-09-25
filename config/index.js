610cbda09aac0cf50bda0e8c3885aa56c459b1ad
啥都不会的铁废物 <98574802+hpu-hyh@users.noreply.github.com>
2022-09-25 19:18:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -78,6 +78,24 @@ const USER_CONFIG = {
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: 'https://wangxinleo.cn',
+    },
+     {
+      // 想要发送的人的名字
+      name: 'H',
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'osP5X69R3XPI-3m88ho9gC2gnZAI',
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: '8MFPY-QWRxMxnScY_93UK62OwtshcHbp6esjIFP7VwE',
+      // 所在省份
+      province: '河南',
+      // 所在城市
+      city: '焦作',
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '12-27',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: 'https://wangxinleo.cn',
     }
   ],
 
